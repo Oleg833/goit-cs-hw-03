4918997d394d3747b9800a5f68d9d99279fb8eb1
Oleg833 <oleg.kovtun447@gmail.com>
2024-03-26 00:29:17 +0200

    upd con

--- connect.py
+++ connect.py
@@ -20,8 +20,8 @@ def create_connect():
     conn = None
     try:
         conn = psycopg2.connect(**conn_params)
-        print("Connection successful")
         try:
+            print("Connection successful")
             yield conn
         except:
             conn.close()
@@ -33,11 +33,34 @@ def create_connect():
             conn.close()
 
 
+def get_data(conn, sql, params=None):
+    data = None
+    cur = conn.cursor()
+    try:
+        cur.execute(sql, params)
+        data = cur.fetchall()
+    except DatabaseError as er:
+        logging.error(f"Database error: {er}")
+    finally:
+        cur.close()
+    return data
+
+
+def get_count_tasks_by_users(conn):
+    sql = """
+    select u.*, coalesce(count(t.user_id), 0) as task_count from users u
+    left join tasks t on t.user_id = u.id
+    group by u.id;
+    """
+
+    return get_data(conn, sql)
+
+
 if __name__ == "__main__":
     load_dotenv(Path(__file__).parent / ".env")
     try:
         with create_connect() as conn:
-            pass
+            print(get_count_tasks_by_users(conn))
     except RuntimeError as er:
         logging.error(f"Runtime error: {er}")
     except DatabaseError as er:
